3aa041c1b931bc33a7963d838537427126476033
Koichiro-Kanaya <38249895+Koichiro-Kanaya@users.noreply.github.com>
2022-03-04 00:01:27 +0900

    Update end.sh

--- end.sh
+++ end.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ip netns delete mon
-ip netns delete user # TODO: delete
-ip netns delete con # TODO: delete
-ip netns delete rt0 # TODO: delete
-ip netns delete rt1 # TODO: delete
-ip netns delete rt2 # TODO: delete 
+ip netns delete user
+ip netns delete con
+ip netns delete rt0
+ip netns delete rt1
+ip netns delete rt2
